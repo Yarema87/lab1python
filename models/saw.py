525d1658d6b76368aa35d8af92519cdb0b5ff2a0
Yarema87 <gekon4uk@gmail.com>
2023-05-25 23:48:08 +0300

    added third lab

--- models/saw.py
+++ models/saw.py
@@ -5,17 +5,22 @@ class Saw(ABC):
     """
     abstract class which describes all other types of saws which are also described in this project
     """
-    def __init__(self, brand, power, worktime, is_working=False):
+    def __init__(self, brand, power, worktime, types_of_supply, is_working=False):
         """
         :param brand: brand of saw
         :param power: power of saw in watts
         :param worktime: how much time (in hours) can this saw work
+        :param types_of_supply: which types of supply are available for this saw
         :param is_working: is this saw working now
         """
         self.brand = brand
         self.power = power
         self.worktime = worktime
+        self.types_of_supply = types_of_supply
         self.is_working = is_working
+
+    def __getitem__(self, item):
+        print(type(item), item)
 
     @abstractmethod
     def get_remaining_working_time(self):
